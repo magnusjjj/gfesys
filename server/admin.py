cb8b46d97cb00713e66dedf9bf93bed30ffcd6e1
root <root@GfeTest.(none)>
2015-03-18 08:23:36 +0000

    Removed unused dependency on ajax_select

--- server/admin.py
+++ server/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from server.models import *
-from ajax_select import make_ajax_form
-from ajax_select.admin import AjaxSelectAdmin
 
 # Register your models here.
 
